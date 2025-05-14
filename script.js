7ed8cea27d36cdce380bf336e8b78e8d3f794cd8
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-15 00:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function firstWord(s) {
   // your code here
-	let output=[];
-	output.push(s.split(' ')[0]);
-	return output
+	// let output=[];
+	// output.push(s.split(' ')[0]);
+	// return output
+	if(s=="" || '' notin s){
+		return s
+	}
 	
 }
 
 // // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s))
+alert(firstWord(s));
