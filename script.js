a088fbe29f6b344c5f730c01f7553aef554ed4ad
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-15 00:07:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function firstWord(s) {
   // your code here
-	// let output=[];
-	// output.push(s.split(' ')[0]);
-	// return output
-	if(s=="" || '' notin s){
-		return s
+	let output=[];
+	output.push(s.split(' ')[0]);
+	return output
+	// if(s=="" || '' === s){
+	// 	return s
 	}
 	
 }
