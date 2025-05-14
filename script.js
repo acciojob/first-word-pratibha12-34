0c60cbb8371d6c6c50f73f0ad95316b8b5770c48
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-14 23:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
   // your code here
+	return s.split('')[0]
 }
 
-// Do not change the code below
+// // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
